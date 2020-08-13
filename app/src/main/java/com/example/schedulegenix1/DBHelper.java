c23a83676afd5a754ada9907719c7b921fa3db9a
aparna-jay <aparna.jayawardena1999@gmail.com>
2020-08-13 15:25:08 +0530

    Database code and Display schedules added

---
+++ app/src/main/java/com/example/schedulegenix1/DBHelper.java
@@ -0,0 +1,103 @@
+package com.example.schedulegenix1;
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "Schedulegenix.db";
+    public static final String SCHEDULES_TABLE_NAME = "schedules";
+    public static final String SCHEDULES_COLUMN_ID = "id";
+    public static final String SCHEDULES_COLUMN_TITLE = "title";
+    public static final String SCHEDULES_COLUMN_DATE = "date";
+    public static final String SCHEDULES_COLUMN_STARTTIME = "starttime";
+    public static final String SCHEDULES_COLUMN_ENDTIME = "endtime";
+    public static final String SCHEDULES_COLUMN_VENUE = "venue";
+    private HashMap hp;
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME , null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        db.execSQL(
+                "create table schedules " +
+                        "(id integer primary key, title text,date text,starttime text, endtime text,venue text)"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO Auto-generated method stub
+        db.execSQL("DROP TABLE IF EXISTS schedules");
+        onCreate(db);
+    }
+
+    public boolean insertSchedule (String title, String date, String starttime, String endtime,String venue) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("title", title);
+        contentValues.put("date", date);
+        contentValues.put("starttime", starttime);
+        contentValues.put("endtime", endtime);
+        contentValues.put("venue", venue);
+        db.insert("schedules", null, contentValues);
+        return true;
+    }
+
+    public Cursor getData(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from schedules where id="+id+"", null );
+        return res;
+    }
+
+    public int numberOfRows(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.queryNumEntries(db, SCHEDULES_TABLE_NAME);
+        return numRows;
+    }
+
+    public boolean updateSchedule (Integer id, String title, String date, String starttime, String endtime,String venue) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("title", title);
+        contentValues.put("date", date);
+        contentValues.put("starttime", starttime);
+        contentValues.put("endtime", endtime);
+        contentValues.put("venue", venue);
+        db.update("schedules", contentValues, "id = ? ", new String[] { Integer.toString(id) } );
+        return true;
+    }
+
+    public Integer deleteSchedule (Integer id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete("schedules",
+                "id = ? ",
+                new String[] { Integer.toString(id) });
+    }
+
+    public ArrayList<String> getAllSchedules() {
+        ArrayList<String> array_list = new ArrayList<String>();
+
+        //hp = new HashMap();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from schedules", null );
+        res.moveToFirst();
+
+        while(res.isAfterLast() == false){
+            array_list.add(res.getString(res.getColumnIndex(SCHEDULES_COLUMN_TITLE)));
+            res.moveToNext();
+        }
+        return array_list;
+    }
+}
---
+++ app/src/main/java/com/example/schedulegenix1/DisplaySchedules.java
@@ -0,0 +1,189 @@
+package com.example.schedulegenix1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DisplaySchedules extends Activity {
+    int from_Where_I_Am_Coming = 0;
+    private DBHelper mydb ;
+
+    TextView Title ;
+    TextView Date;
+    TextView StartTime;
+    TextView EndTime;
+    TextView Venue;
+    int id_To_Update = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_schedules);
+        Title = (TextView) findViewById(R.id.editTextTitle);
+        Date = (TextView) findViewById(R.id.editTextDate);
+        StartTime = (TextView) findViewById(R.id.editTextStartTime);
+        EndTime = (TextView) findViewById(R.id.editTextEndTime);
+        Venue = (TextView) findViewById(R.id.editTextVenue);
+
+        mydb = new DBHelper(this);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras !=null) {
+            int Value = extras.getInt("id");
+
+            if(Value>0){
+                //means this is the view part not the add contact part.
+                Cursor rs = mydb.getData(Value);
+                id_To_Update = Value;
+                rs.moveToFirst();
+
+                String Titl = rs.getString(rs.getColumnIndex(DBHelper.SCHEDULES_COLUMN_TITLE));
+                String Dat = rs.getString(rs.getColumnIndex(DBHelper.SCHEDULES_COLUMN_DATE));
+                String StartTim = rs.getString(rs.getColumnIndex(DBHelper.SCHEDULES_COLUMN_STARTTIME));
+                String EndTim = rs.getString(rs.getColumnIndex(DBHelper.SCHEDULES_COLUMN_ENDTIME));
+                String Venu = rs.getString(rs.getColumnIndex(DBHelper.SCHEDULES_COLUMN_VENUE));
+
+                if (!rs.isClosed())  {
+                    rs.close();
+                }
+                Button b = (Button)findViewById(R.id.button1);
+                b.setVisibility(View.INVISIBLE);
+
+                Title.setText((CharSequence)Titl);
+                Title.setFocusable(false);
+                Title.setClickable(false);
+
+                Date.setText((CharSequence)Dat);
+                Date.setFocusable(false);
+                Date.setClickable(false);
+
+                StartTime.setText((CharSequence)StartTim);
+                StartTime.setFocusable(false);
+                StartTime.setClickable(false);
+
+                EndTime.setText((CharSequence)EndTim);
+                EndTime.setFocusable(false);
+                EndTime.setClickable(false);
+
+                Venue.setText((CharSequence)Venu);
+                Venue.setFocusable(false);
+                Venue.setClickable(false);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        Bundle extras = getIntent().getExtras();
+
+        if(extras !=null) {
+            int Value = extras.getInt("id");
+            if(Value>0){
+                getMenuInflater().inflate(R.menu.display_schedule, menu);
+            } else{
+                getMenuInflater().inflate(R.menu.main_menu, menu);
+            }
+        }
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(item.getItemId()) {
+            case R.id.Edit_Schedule:
+                Button b = (Button)findViewById(R.id.button1);
+                b.setVisibility(View.VISIBLE);
+                Title.setEnabled(true);
+                Title.setFocusableInTouchMode(true);
+                Title.setClickable(true);
+
+                Date.setEnabled(true);
+                Date.setFocusableInTouchMode(true);
+                Date.setClickable(true);
+
+                StartTime.setEnabled(true);
+                StartTime.setFocusableInTouchMode(true);
+                StartTime.setClickable(true);
+
+                EndTime.setEnabled(true);
+                EndTime.setFocusableInTouchMode(true);
+                EndTime.setClickable(true);
+
+                Venue.setEnabled(true);
+                Venue.setFocusableInTouchMode(true);
+                Venue.setClickable(true);
+
+                return true;
+            case R.id.Delete_Schedule:
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(R.string.deleteSchedule)
+                        .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                mydb.deleteSchedule(id_To_Update);
+                                Toast.makeText(getApplicationContext(), "Deleted Successfully",
+                                        Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                                startActivity(intent);
+                            }
+                        })
+                        .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User cancelled the dialog
+                            }
+                        });
+
+                AlertDialog d = builder.create();
+                d.setTitle("Are you sure");
+                d.show();
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    public void run(View view) {
+        Bundle extras = getIntent().getExtras();
+        if(extras !=null) {
+            int Value = extras.getInt("id");
+            if(Value>0){
+                if(mydb.updateSchedule(id_To_Update,Title.getText().toString(),
+                        Date.getText().toString(), StartTime.getText().toString(),
+                        EndTime.getText().toString(), Venue.getText().toString())){
+                    Toast.makeText(getApplicationContext(), "Updated", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                } else{
+                    Toast.makeText(getApplicationContext(), "not Updated", Toast.LENGTH_SHORT).show();
+                }
+            } else{
+                if(mydb.insertSchedule(Title.getText().toString(), Date.getText().toString(),
+                        StartTime.getText().toString(), EndTime.getText().toString(),
+                        Venue.getText().toString())){
+                    Toast.makeText(getApplicationContext(), "done",
+                            Toast.LENGTH_SHORT).show();
+                } else{
+                    Toast.makeText(getApplicationContext(), "not done",
+                            Toast.LENGTH_SHORT).show();
+                }
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        }
+    }
+}
