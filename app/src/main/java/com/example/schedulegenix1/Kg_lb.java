65e0fce924bd28d53223b482b9b0b8a570095439
aparna-jay <aparna.jayawardena1999@gmail.com>
2020-08-16 15:09:28 +0530

    Kg - lb converter created

---
+++ app/src/main/java/com/example/schedulegenix1/Kg_lb.java
@@ -0,0 +1,14 @@
+package com.example.schedulegenix1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Kg_lb extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kg_lb);
+    }
+}
--- app/src/main/java/com/example/schedulegenix1/UnitConverter.java
+++ app/src/main/java/com/example/schedulegenix1/UnitConverter.java
@@ -2,13 +2,24 @@ package com.example.schedulegenix1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UnitConverter extends AppCompatActivity {
-
+    Button mass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unit_converter);
+        mass = findViewById(R.id.massbutton);
+        mass.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(UnitConverter.this, Kg_lb.class);
+                startActivity(intent);
+            }
+        });
     }
 }
